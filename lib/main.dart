ebe68618daad9453746981db01dfb6ad5da1a7e4
Milan Vadher <milanvadher1996@gmail.com>
2019-08-22 18:49:33 +0530

    Custom spash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:lucky_draw_revamp/src/ui/home.dart';
-import 'package:lucky_draw_revamp/src/ui/login.dart';
+import 'package:lucky_draw_revamp/src/ui/splashscreen.dart';
 import 'package:lucky_draw_revamp/src/utils/cachedata.dart';
 import 'package:lucky_draw_revamp/src/utils/config.dart';
 import 'src/app.dart';
 
 void main() async {
-  Widget homepage = LoginPage();
-  bool isLogin = await Config.isLogin();
   CacheData.isDarkTheme = await Config.isDarkMode();
-  if (isLogin) {
-    homepage = HomePage();
-  }
   runApp(MyApp(
-    homepage: homepage,
+    homepage: SplashScreen(),
   ));
 }
---
+++ lib/src/ui/splashscreen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:lucky_draw_revamp/src/ui/home.dart';
+import 'package:lucky_draw_revamp/src/ui/login.dart';
+import 'package:lucky_draw_revamp/src/utils/common_widget.dart';
+import 'package:lucky_draw_revamp/src/utils/config.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  initData() async {
+    Widget homepage = LoginPage();
+    bool isLogin = await Config.isLogin();
+    if (isLogin) {
+      homepage = HomePage();
+    }
+    // Navigator.pushReplacement(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => homepage),
+    // );
+  }
+
+  @override
+  void initState() {
+    initData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height - 25,
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 40),
+                    child: Center(
+                      child: Image.asset('images/logo.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ListView(
+              children: <Widget>[
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: MediaQuery.of(context).size.height - 25,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 400),
+                        child: Text(
+                          'LuckyDraw',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: Center(
+                          child: CommonWidget.progressIndicator(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
