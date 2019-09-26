f75d35b6ca999323ab44a017cd5c96b5dd63f660
kamlesh9070 <kamlesh9070@gmail.comm>
2019-09-26 23:39:46 +0530

    app_setting

---
+++ lib/src/model/app_setting.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_setting.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AppSetting _$AppSettingFromJson(Map<String, dynamic> json) {
+  return AppSetting()
+    ..appVersion = json['appversion'] as String
+    ..iosAppVersion = json['ios_appversion'] as String
+    ..isLuckyDrawActive = json['is_luckydraw_active'] as bool ?? false
+    ..akramYouthURL = json['akram_youth_url'] as String ??
+        'https://youth.dadabhagwan.org/gallery/akram-youth/#app/'
+    ..regURL = json['reg_url'] as String ?? 'http://gncevents.page.link/reg/';
+}
+
+Map<String, dynamic> _$AppSettingToJson(AppSetting instance) =>
+    <String, dynamic>{
+      'appversion': instance.appVersion,
+      'ios_appversion': instance.iosAppVersion,
+      'is_luckydraw_active': instance.isLuckyDrawActive,
+      'akram_youth_url': instance.akramYouthURL,
+      'reg_url': instance.regURL,
+    };
