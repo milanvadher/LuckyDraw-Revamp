ccec88f424fc90ff87a14419926bd9c7a5e11cb1
Milan Vadher <milanvadher1996@gmail.com>
2020-02-21 11:03:46 +0530

    change to live url  ...

--- lib/src/utils/constant.dart
+++ lib/src/utils/constant.dart
@@ -3,10 +3,10 @@ import 'package:intl/intl.dart';
 
 // API END-POINT
 //const String apiUrl = 'http://3.16.51.94:60371'; // Node URL
-const String apiUrl = 'http://3.16.51.94:3000'; // Python URL
-const String ayApiUrl = 'http://3.16.51.94:3001'; // Python URL GNANG
-// const String apiUrl = 'http://luckydrawapi.dbf.ooo:8000/'; // Live URL
-// const String ayApiUrl = 'http://luckydrawapi.dbf.ooo:3001/'; // Live URL GNANG
+// const String apiUrl = 'http://3.16.51.94:3000'; // Python URL
+// const String ayApiUrl = 'http://3.16.51.94:3001'; // Python URL GNANG
+const String apiUrl = 'http://luckydrawapi.dbf.ooo:8000'; // Live URL
+const String ayApiUrl = 'http://luckydrawapi.dbf.ooo:3001'; // Live URL GNANG
 
 // Fix-Headers
 const Map<String, String> headers = {'content-type': 'application/json'};
